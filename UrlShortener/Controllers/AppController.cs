e48cdc05a8c5b32c6284b0a1eae832fd79d8b915
LeuisKen <leuisken@gmail.com>
2023-12-18 11:13:30 +0100

    add catch handler

--- UrlShortener/Controllers/AppController.cs
+++ UrlShortener/Controllers/AppController.cs
@@ -7,19 +7,13 @@ namespace UrlShortener.Controllers;
 
 [ApiController]
 [Route("app/api", Name = "UrlShortenerManagement")]
-public class AppController : ControllerBase
+public class AppController(
+    IDynamoDBContext context,
+    ILogger<AppController> logger
+) : ControllerBase
 {
-    private readonly IDynamoDBContext _context;
-    private readonly ILogger<AppController> _logger;
-
-    public AppController(
-        IDynamoDBContext context,
-        ILogger<AppController> logger
-    )
-    {
-        _context = context;
-        _logger = logger;
-    }
+    private readonly IDynamoDBContext _context = context;
+    private readonly ILogger<AppController> _logger = logger;
 
     private async Task<string> GenerateAlias()
     {
@@ -35,59 +29,69 @@ public class AppController : ControllerBase
     [HttpPost("createUrl", Name = "CreateUrl")]
     public async Task<ActionResult<Url>> PostUrl(UrlDTO urlDTO)
     {
+        try {
+            var hasCustomAlias = !string.IsNullOrEmpty(urlDTO.CustomAlias);
 
-        var hasCustomAlias = !string.IsNullOrEmpty(urlDTO.CustomAlias);
-
-        if (hasCustomAlias)
-        {
-            var existingUrl = await _context.LoadAsync<Url>(urlDTO.CustomAlias);
-            if (existingUrl != null)
+            if (hasCustomAlias)
             {
-                return BadRequest(new {
-                    status = 400,
-                    msg = "The custom alias conflict with existing short links."
-                });
-            }
-        }
-        else {
-            urlDTO.CustomAlias = await GenerateAlias();
-        }
-
-        var hasExpireDate = !string.IsNullOrEmpty(urlDTO.ExpireDate);
-
-        if (hasExpireDate)
-        {
-            var expireDate = DateTime.Parse(urlDTO.ExpireDate!);
-            if (expireDate < DateTime.Now)
-            {
-                return BadRequest(
-                    new {
+                var existingUrl = await _context.LoadAsync<Url>(urlDTO.CustomAlias);
+                if (existingUrl != null)
+                {
+                    return BadRequest(new {
                         status = 400,
-                        msg = "The expire date must be greater than current date."
-                    }
-                );
+                        msg = "The custom alias conflict with existing short links."
+                    });
+                }
             }
-        }
-        else {
-            urlDTO.ExpireDate = DateTime.Now.AddYears(5).ToString();
-        }
+            else {
+                urlDTO.CustomAlias = await GenerateAlias();
+            }
 
-        var todoItem = new Url
+            var hasExpireDate = !string.IsNullOrEmpty(urlDTO.ExpireDate);
+
+            if (hasExpireDate)
+            {
+                var expireDate = DateTime.Parse(urlDTO.ExpireDate!);
+                if (expireDate < DateTime.Now)
+                {
+                    return BadRequest(
+                        new {
+                            status = 400,
+                            msg = "The expire date must be greater than current date."
+                        }
+                    );
+                }
+            }
+            else {
+                urlDTO.ExpireDate = DateTime.Now.AddYears(5).ToString();
+            }
+
+            var todoItem = new Url
+            {
+                OriginalUrl = urlDTO.OriginalUrl,
+                Alias = urlDTO.CustomAlias!,
+                CreateTime = DateTime.Now.ToString(),
+                ExpireDate = urlDTO.ExpireDate!,
+            };
+
+            await _context.SaveAsync(todoItem);
+
+            return Ok(new {
+                status = 0,
+                msg = "",
+                data = new {
+                    url = $"{Request.Scheme}://{Request.Host}/{todoItem.Alias}"
+                }
+            });
+        }
+        catch (Exception ex)
         {
-            OriginalUrl = urlDTO.OriginalUrl,
-            Alias = urlDTO.CustomAlias!,
-            CreateTime = DateTime.Now.ToString(),
-            ExpireDate = urlDTO.ExpireDate!,
-        };
+            _logger.LogError(ex, "Error when creating short link");
+            return BadRequest(new {
+                status = 500,
+                msg = "Please contact the administrator for help."
+            });
+        }
 
-        await _context.SaveAsync(todoItem);
-
-        return Ok(new {
-            status = 0,
-            msg = "",
-            data = new {
-                url = $"{Request.Scheme}://{Request.Host}/{todoItem.Alias}"
-            }
-        });
     }
 }
